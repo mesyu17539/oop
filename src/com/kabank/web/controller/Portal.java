c25619130d39fafc7dee048f98ed592fbf2e2736
mesyu17539 <mesyu17539@gmail.com>
2017-11-29 11:22:30 +0900

    오전 11:22 2017-11-29

--- src/com/kabank/web/controller/Portal.java
+++ src/com/kabank/web/controller/Portal.java
@@ -8,8 +8,8 @@ import com.kabank.web.service.MemberService;
 public class Portal {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String name = "", sex = "",ssn="";
-		int number = 0,age=0;
+		String name = "";
+		int number = 101;
 		while (true) {
 			System.out.println("[MENU] [0] 종료 [1] 회원가입 [2] 계좌계설");
 			switch (scanner.nextInt()) {
@@ -19,11 +19,21 @@ public class Portal {
 			case 1:
 				System.out.println("이름");
 				name = scanner.next();
-				System.out.println("주민번호");
 				MemberService memberService = new MemberService();
-				String[] str= memberService.createAge(scanner.next());
-				System.out.printf("[고객번호] %s" + "[이름] %s" + "[성별] %s [나이] %s",str[0], name, str[1], str[2]);
-				number++;
+				String ssn="";
+				System.out.println("주민번호");
+				boolean bool = true;
+				while (bool) {
+					ssn = scanner.next();
+					if (ssn.charAt(7) == '0' || ssn.charAt(7) == '1') {
+						String[] str = memberService.createAge(ssn, number);
+						System.out.printf("[고객번호] %s [이름] %s [성별] %s [나이] %s", str[0], name, str[1], str[2]);
+						number = Integer.parseInt(str[0])+1;
+						bool = false;
+					}else {
+						System.out.println("다시입력하세요");
+					}
+				}
 				break;
 			case 2:
 				AccountService accountService = new AccountService();
--- src/com/kabank/web/service/MemberService.java
+++ src/com/kabank/web/service/MemberService.java
@@ -1,9 +1,8 @@
 package com.kabank.web.service;
 
 public class MemberService {
-	public String[] createAge(String ssn) {
+	public String[] createAge(String ssn, int number) {
 		String[] str = new String[3];
-		
 		switch (ssn.charAt(7)) {
 		case '1':
 			str[1] = "남자";
@@ -11,12 +10,9 @@ public class MemberService {
 		case '2':
 			str[1] = "여자";
 			break;
-		default:
-			str[1] = "잘못됬어";
-			break;
 		}
-		str[0]="101";
-		str[2]=ssn.charAt(0)+""+ssn.charAt(1);
+		str[0]=number+"";
+		str[2]=118-Integer.parseInt(ssn.charAt(0)+""+ssn.charAt(1))+"\n";
 		return str;
 	}
 }
